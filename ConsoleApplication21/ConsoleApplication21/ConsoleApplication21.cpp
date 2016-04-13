6424e817ae44f68dbe4693fd9d67b440c318baf3
A <annmozer116@gmail.com>
2016-04-13 17:16:25 +0300

    Import project
    import

---
+++ ConsoleApplication21/ConsoleApplication21/ConsoleApplication21.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication21.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+void hello()
+{
+	printf("Hello, GitHub!\n");
+}
+int _tmain(int argc, _TCHAR* argv[])
+{
+	hello();
+	return 0;
+}
+
